d36ea3a10fa51d7bc15046c3d82273c90c8d9b2d
Dayeol Lee <dayeol@berkeley.edu>
2020-10-13 00:10:36 +0000

    Add Keystone security monitor as a submodule
    - Copy relevant headers to include dir
    - Change generic platform to init SM in final_init()

--- platform/generic/platform.c
+++ platform/generic/platform.c
@@ -20,6 +20,7 @@
 #include <sbi_utils/timer/fdt_timer.h>
 #include <sbi_utils/ipi/fdt_ipi.h>
 #include <sbi_utils/reset/fdt_reset.h>
+#include <sbi_utils/experimental/keystone/sm.h>
 
 extern const struct platform_override sifive_fu540;
 
@@ -154,6 +155,8 @@ static int generic_final_init(bool cold_boot)
 		if (rc)
 			return rc;
 	}
+
+	sm_init();
 
 	return 0;
 }
